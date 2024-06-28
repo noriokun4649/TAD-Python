e83a07a6c1b62b4075a783f30295ff71761b19c7
noriokun4649 <noriokun.official.mail@gmail.com>
2024-06-29 02:27:23 +0900

    Add pagenation.

--- download.py
+++ download.py
@@ -13,11 +13,26 @@ def get_existing_video_ids(directory):
     return numbers
 
 
-def fetch_twitch_video_ids(url, headers):
+def fetch_user_name(user_id, headers):
+    url = f"https://api.twitch.tv/helix/users?id={user_id}"
     response = requests.get(url, headers=headers)
     data = response.json()
-    return [item['id'] for item in data['data']]
+    return data['data'][0]['display_name']
 
+
+def fetch_twitch_video_ids(url, headers):
+    video_ids = []
+    baseurl = url
+    while True:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        video_ids.extend([item['id'] for item in data['data']])
+        cursor = data.get('pagination', {}).get('cursor')
+        if cursor:
+            url = f"{baseurl}&after={cursor}"
+        else:
+            break
+    return video_ids
 
 def download_video(video_id):
     ydl_opts = {
@@ -33,32 +48,31 @@ def download_videos_concurrently(video_ids, max_workers):
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         executor.map(download_video, video_ids)
 
+userid = os.getenv('DL_USER_ID')
+download_dir = os.getenv('DL_DIR',os.path.join(os.getcwd(), 'Download'))
 
-def stop_workers(executor):
-    executor.shutdown(wait=False)
-    print("すべてのWorkerを中止しました。")
-
-
-download_dir = str(os.getenv('DL_DIR',os.path.join(os.getcwd(), 'Download')))
-os.makedirs(download_dir, exist_ok=True)
+os.makedirs(download_dir, exist_ok=True)C
 os.chdir(download_dir)
 
 existing_video_ids = get_existing_video_ids(download_dir)
 
-url = f"https://api.twitch.tv/helix/videos?user_id={os.getenv('DL_USEE_ID')}&period=month&first=100"
+url = f"https://api.twitch.tv/helix/videos?user_id={userid}&first=100"
 headers = {
     'Client-ID': os.getenv('TWITCH_CLIENT_ID'),
     'Authorization': f'Bearer {os.getenv("TWITCH_AUTH_TOKEN")}'
 }
 
 twitch_video_ids = fetch_twitch_video_ids(url, headers)
+user_name = fetch_user_name(userid, headers)
 
-print("投稿されているアーカイブ一覧！！！")
+print(f"{user_name}({userid})のダウンロード開始！！！")
+
+print(f"\n投稿されているアーカイブ一覧！！！{len(twitch_video_ids)}個！")
 print(twitch_video_ids)
 
 new_video_ids = [video_id for video_id in twitch_video_ids if video_id not in existing_video_ids]
 
-print("\n未ダウンロードのアーカイブ一覧！！！")
+print(f"\n未ダウンロードのアーカイブ一覧！！！{len(new_video_ids)}個！")
 print(new_video_ids)
 print(f"{len(new_video_ids)}個のアーカイブが未DL")
 
