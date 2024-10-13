d810032b46bbd5856b61777fec330b294297db94
noriokun4649 <noriokun.official.mail@gmail.com>
2024-10-14 05:55:23 +0900

    Add username DL.

--- download.py
+++ download.py
@@ -6,12 +6,16 @@ from dotenv import load_dotenv
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-load_dotenv()
-
 def get_existing_video_ids(directory):
     pattern = re.compile(r'\[v(\d+)\]\.mp4$')
     numbers = [pattern.search(file).group(1) for file in os.listdir(directory) if pattern.search(file)]
     return numbers
+
+def fetch_user_id(username, headers):
+    url = f"https://api.twitch.tv/helix/users?login={username}"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data['data'][0]['id']
 
 
 def fetch_user_name(user_id, headers):
@@ -35,6 +39,7 @@ def fetch_twitch_video_ids(url, headers):
             break
     return video_ids
 
+
 def download_video(video_id):
     ydl_opts = {
                     'format': 'best',
@@ -49,37 +54,68 @@ def download_videos_concurrently(video_ids, max_workers):
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         executor.map(download_video, video_ids)
 
-userid = os.getenv('DL_USER_ID')
-download_dir = os.getenv('DL_DIR',os.path.join(os.getcwd(), 'Download'))
 
-os.makedirs(download_dir, exist_ok=True)
-os.chdir(download_dir)
+def fetch_oauth_token():
+    url = "https://id.twitch.tv/oauth2/token"
+    clinet_id = os.getenv('TWITCH_CLIENT_ID')
+    client_secret = os.getenv('TWITCH_CLIENT_SECRET')
+    url = url + f"?client_id={clinet_id}&client_secret={client_secret}&grant_type=client_credentials"
+    response = requests.post(url=url)
+    data = response.json()
+    return data["access_token"]
 
-existing_video_ids = get_existing_video_ids(download_dir)
 
-url = f"https://api.twitch.tv/helix/videos?user_id={userid}&first=100"
-headers = {
-    'Client-ID': os.getenv('TWITCH_CLIENT_ID'),
-    'Authorization': f'Bearer {os.getenv("TWITCH_AUTH_TOKEN")}'
-}
+def init():
+    userid = os.getenv('DL_USER_ID')
+    client_id = os.getenv('TWITCH_CLIENT_ID')
+    download_dir = os.getenv('DL_DIR',os.path.join(os.getcwd(), 'Download'))
+    auth_token = fetch_oauth_token()
+    headers = {
+        'Client-ID': client_id,
+        'Authorization': f'Bearer {auth_token}'
+    }
 
-twitch_video_ids = fetch_twitch_video_ids(url, headers)
-user_name = fetch_user_name(userid, headers)
+    if userid == None:
+        print("UserIDが空です")
+        return
 
-print(f"{user_name}({userid})のダウンロード開始！！！ [{datetime.now()}]")
+    if not userid.isdigit():
+        userid = fetch_user_id(userid, headers)
 
-print(f"\n投稿されているアーカイブ一覧！！！{len(twitch_video_ids)}個！")
-print(twitch_video_ids)
+    os.makedirs(download_dir, exist_ok=True)
+    os.chdir(download_dir)
 
-new_video_ids = [video_id for video_id in twitch_video_ids if video_id not in existing_video_ids]
+    existing_video_ids = get_existing_video_ids(download_dir)
 
-print(f"\n未ダウンロードのアーカイブ一覧！！！{len(new_video_ids)}個！")
-print(new_video_ids)
-print(f"{len(new_video_ids)}個のアーカイブが未DL")
+    url = f"https://api.twitch.tv/helix/videos?user_id={userid}&first=100"
 
-max_workers = int(os.getenv('MAX_WORKERS', 4))
 
-print(f"\n{max_workers}スレッドで動画ダウンロード開始！！！")
-download_videos_concurrently(new_video_ids, max_workers)
+    twitch_video_ids = fetch_twitch_video_ids(url, headers)
+    user_name = fetch_user_name(userid, headers)
 
-print("\nダウンロード終了！！！\n--------------------------\n")
+    print(f"{user_name}({userid})のダウンロード開始！！！ [{datetime.now()}]")
+
+    print(f"\n投稿されているアーカイブ一覧！！！{len(twitch_video_ids)}個！")
+    print(twitch_video_ids)
+
+    new_video_ids = [video_id for video_id in twitch_video_ids if video_id not in existing_video_ids]
+
+    if len(new_video_ids) < 1:
+        print("未ダウンロードのアーカイブ無し！！！")
+        return
+
+    print(f"\n未ダウンロードのアーカイブ一覧！！！{len(new_video_ids)}個！")
+    print(new_video_ids)
+    print(f"{len(new_video_ids)}個のアーカイブが未DL")
+
+    max_workers = int(os.getenv('MAX_WORKERS', 4))
+
+    print(f"\n{max_workers}スレッドで動画ダウンロード開始！！！")
+    download_videos_concurrently(new_video_ids, max_workers)
+
+    print("\nダウンロード終了！！！\n--------------------------\n")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    init()
